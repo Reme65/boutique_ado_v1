ab0332e7b1d23520d99cf71bd02cab68bda01864
Reme65 <amreme168@gmail.com>
2026-08-09 23:55:42 +0100

    modify products/views.py

--- products/views.py
+++ products/views.py
@@ -3,11 +3,11 @@ from .models import Product
 
 # Create your views here.
 
+
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-
     products = Product.objects.all()
-    
+
     context = {
         'products': products
     }
