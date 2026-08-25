40d848f63e39826d1f94d5cd8750bfdd5d9c14f2
Reme65 <amreme168@gmail.com>
2026-08-25 21:52:00 +0100

    Fix bag template layouts and resolve quantity dictionary session bug

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -3,6 +3,9 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    if price is None:
+        return 0
     return price * quantity
