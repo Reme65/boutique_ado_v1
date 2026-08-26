b90d5208fb5efc816252901ef6544ea85b6f7083
Reme65 <amreme168@gmail.com>
2026-08-26 20:43:44 +0100

    Successfully install and configure Stripe payment gateway integration on first attempt

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,10 @@
+import os
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -8,12 +12,16 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51U8PgZ0Zd1U0heuCwkS3WSLlKeVdT6jUPIz3hYz5aDlHrIGhfNCL45Y3UMO1fxSdIE1LquZ03KEMaWeKHrlmv5I500Pk7QRWkd',
-        'client_secret': 'test client secret',
+        'stripe_public_key': os.environ.get('STRIPE_PUBLIC_KEY'),
+        'client_secret': os.environ.get('STRIPE_SECRET_KEY'),
     }
 
     return render(request, template, context)
